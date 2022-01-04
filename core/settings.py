dd14910c49ffcc779077bbe6e1134996a460935a
Ananay Srivastava <ananaysrivastava5@gmail.com>
2022-01-04 12:46:30 -0800

    accounts models added and User signup with email confirmation

--- core/settings.py
+++ core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     
     'store',
     'basket',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,12 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom user model
+AUTH_USER_MODEL = 'account.UserBase'
+LOGIN_REDIRECT_URL = '/account/dashboard'
+LOGIN_URL = '/account/login/'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
